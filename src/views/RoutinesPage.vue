8b7663c97a9a1ff963eb8bd585fc64ae34aca406
Sean Wong <seanwong24@live.com>
2025-02-14 21:57:26 +0000

    modify header date display

--- src/views/RoutinesPage.vue
+++ src/views/RoutinesPage.vue
@@ -4,28 +4,30 @@
       <ion-toolbar>
         <ion-title>
           {{
-            daysOfCurrentWeek?.[0]
+            daysOfCurrentWeek?.at(0)
               ?.add({
                 weeks: currentPageIndex + 1 - pageCount,
               })
               .toLocaleString(locale)
-          }}</ion-title
-        >
+          }}
+          <br />
+          <ion-note>
+            to {{
+              daysOfCurrentWeek?.at(-1)
+                ?.add({
+                  weeks: currentPageIndex + 1 - pageCount,
+                })
+                .toLocaleString(locale)
+            }}
+          </ion-note>
+        </ion-title>
         <ion-buttons slot="end">
-          <ion-button
-            v-if="!editingViewEnabled"
-            title="Previous Week"
-            @click="swiperRef?.swiper.slidePrev()"
-            :disabled="isFirstSlideActive"
-          >
+          <ion-button v-if="!editingViewEnabled" title="Previous Week" @click="swiperRef?.swiper.slidePrev()"
+            :disabled="isFirstSlideActive">
             <ion-icon slot="icon-only" :icon="arrowBack"></ion-icon>
           </ion-button>
-          <ion-button
-            v-if="!editingViewEnabled"
-            title="Next Week"
-            @click="swiperRef?.swiper.slideNext()"
-            :disabled="isLastSlideActive"
-          >
+          <ion-button v-if="!editingViewEnabled" title="Next Week" @click="swiperRef?.swiper.slideNext()"
+            :disabled="isLastSlideActive">
             <ion-icon slot="icon-only" :icon="arrowForward"></ion-icon>
           </ion-button>
           <ion-button title="Toggle View">
@@ -35,69 +37,55 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <div
-        style="
+      <div style="
           min-height: 100%;
           display: grid;
           grid-template-rows: auto auto 1fr;
           grid-template-columns: 100%;
-        "
-      >
+        ">
         <ion-header collapse="condense" style="grid-row: 1">
           <ion-toolbar>
-            <ion-title size="large">
+            <ion-title>
               {{
-                daysOfCurrentWeek?.[0]
+                daysOfCurrentWeek?.at(0)
                   ?.add({
                     weeks: currentPageIndex + 1 - pageCount,
                   })
                   .toLocaleString(locale)
-              }}</ion-title
-            >
+              }}
+              -
+              {{
+                daysOfCurrentWeek?.at(-1)
+                  ?.add({
+                    weeks: currentPageIndex + 1 - pageCount,
+                  })
+                  .toLocaleString(locale)
+              }}
+            </ion-title>
           </ion-toolbar>
         </ion-header>
-        <ion-item
-          style="
+        <ion-item style="
             position: sticky;
             z-index: 10;
             top: 0;
             width: 100%;
             grid-row: 2;
-          "
-        >
-          <WeekHeader
-            :days="getAdjustedDays(currentPageIndex + 1 - pageCount)"
-          />
+          ">
+          <WeekHeader :days="getAdjustedDays(currentPageIndex + 1 - pageCount)" />
         </ion-item>
-        <swiper-container
-          ref="swiperRef"
-          virtual="true"
-          style="width: 100%; grid-row: 3"
-          @swiperslidechangetransitionend="handleSlideChange"
-        >
+        <swiper-container ref="swiperRef" virtual="true" style="width: 100%; grid-row: 3"
+          @swiperslidechangetransitionend="handleSlideChange">
           <swiper-slide v-for="i in pageCount" :key="i" :virtualIndex="i">
             <ion-list>
               <div v-if="appData?.routines?.length <= 0">
                 Click bottom right + to add new routine.
               </div>
-              <ion-reorder-group
-                :disabled="!editingViewEnabled"
-                @ionItemReorder="handleReorder"
-              >
-                <ion-item
-                  v-for="(routine, index) in appData.routines"
-                  :key="routine.name"
-                >
-                  <ion-checkbox
-                    v-if="editingViewEnabled"
-                    slot="start"
-                    v-model="routineSelections[index]"
-                  ></ion-checkbox>
-                  <WeekItem
-                    :days="getAdjustedDays(i - pageCount)"
-                    :editingViewEnabled="editingViewEnabled"
-                    v-model:routine="(appData as AppData).routines[index]"
-                  />
+              <ion-reorder-group :disabled="!editingViewEnabled" @ionItemReorder="handleReorder">
+                <ion-item v-for="(routine, index) in appData.routines" :key="routine.name">
+                  <ion-checkbox v-if="editingViewEnabled" slot="start"
+                    v-model="routineSelections[index]"></ion-checkbox>
+                  <WeekItem :days="getAdjustedDays(i - pageCount)" :editingViewEnabled="editingViewEnabled"
+                    v-model:routine="(appData as AppData).routines[index]" />
                   <ion-reorder slot="end"></ion-reorder>
                 </ion-item>
               </ion-reorder-group>
@@ -105,12 +93,7 @@
           </swiper-slide>
         </swiper-container>
       </div>
-      <ion-fab
-        slot="fixed"
-        horizontal="end"
-        vertical="bottom"
-        @click="toggleEditingView"
-      >
+      <ion-fab slot="fixed" horizontal="end" vertical="bottom" @click="toggleEditingView">
         <ion-fab-button title="fab-list-toggle">
           <ion-icon :icon="pencil"></ion-icon>
         </ion-fab-button>
@@ -145,6 +128,7 @@ import {
   IonFabList,
   IonReorderGroup,
   IonCheckbox,
+  IonNote,
   ItemReorderEventDetail,
   alertController,
   onIonViewDidEnter,
